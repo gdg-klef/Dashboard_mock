9b54345fbac40886b08cfa0a5e8e341dd20d1ed0
Adnan Ashraf <ashrafadnan959@gmail.com>
2025-01-06 11:20:50 +0530

    Dashboard

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import {TicketDashboard} from './Pages/TicketDashboard'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Dashboard } from './Pages/SocialMedia'
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<TicketDashboard />} />
+            <Route path='/social' element={<Dashboard/>}/>
+          </Routes>
+      </BrowserRouter>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/BarChart.jsx
@@ -0,0 +1,100 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Bar, BarChart, XAxis, YAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card"
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "./ui/chart"
+
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+    color: "hsl(var(--chart-2))",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+};
+
+
+const chartData = [
+  { browser: "IG", visitors: 230, fill: "var(--color-chrome)" },
+  { browser: "FB", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "X", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+]
+
+
+export function BarChartComponent() {
+  return (
+    <Card className="mr-[9rem]  flex flex-col">
+      <CardHeader>
+        <CardTitle>Reach on Various Platforms</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            layout="vertical"
+            margin={{
+              left: 0,
+            }}
+          >
+            <YAxis
+              dataKey="browser"
+              type="category"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+            />%
+            <XAxis dataKey="visitors" type="number" hide />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Bar dataKey="visitors" layout="vertical" radius={5} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
